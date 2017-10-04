5d75516fc6fd7cb89130dead41125f26306b3950
awindahl <awindahl@kth.se>
2017-10-04 10:43:38 +0200

    neighbour function bug

--- hasNeighbour.java
+++ hasNeighbour.java
@@ -1,6 +1,80 @@
 public class hasNeighbour {
 
-	public boolean (subject[][] grid) {
-		
+	public boolean check  (subject[][] grid) {
+
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j<grid.length; j++){
+
+
+				//check top row
+				if ( i>=1 && j >= 1) {
+
+					if (grid[i-1][j-1] != null) {
+						if (grid[i-1][j-1].getSick()) {
+							return true;
+						}
+					}
+
+					if (grid[i-1][j] != null) {
+						if (grid[i-1][j].getSick()) {
+							return true;
+						}
+					}
+				}
+
+				if (i >= 1 && j < grid.length-1) {
+					if (grid[i-1][j+1] != null) {
+						if (grid[i-1][j+1].getSick()) {
+							return true;
+						}
+					}
+				}
+
+				//check middle row
+				if (j>= 1) {
+
+					if (grid[i][j-1] != null) {
+						if (grid[i][j-1].getSick()) {
+							return true;
+						}
+					}
+				}
+
+				if (j < grid.length-1) {
+					if (grid[i][j+1] != null) {
+						if (grid[i][j+1].getSick()) {
+							return true;
+						}
+					}
+				}
+
+				// check bottom row
+
+				if (i < grid.length-1 && j >= 1) {
+					if (grid[i+1][j-1] != null) {
+						if (grid[i+1][j-1].getSick()) {
+							return true;
+						}
+					}
+				}
+
+				if (i < grid.length-1 && j < grid.length-1) {
+
+
+					if (grid[i+1][j] != null) {
+						if (grid[i+1][j].getSick()) {
+							return true;
+						}
+					}
+
+					if (grid[i+1][j+1] != null) {
+						if (grid[i+1][j+1].getSick()) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
 	}
 }
--- simulation.java
+++ simulation.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class simulation extends infection {
 
-	int day = 0;
+	int day = 1;
 	int totalNumberOfInfected = 0;
 	int totalNumberOfDead = 0;
 
@@ -22,19 +22,19 @@ public class simulation extends infection {
 
 		int peopleRecovered = 0;
 
+		hasNeighbour hn = new hasNeighbour();
+
 
 		Scanner sc = new Scanner(System.in);
 
 		for (int i = 0; i < grid.length; i++) {
 			for (int j = 0; j < grid.length; j++) {
 
-				if (hasNeighbour(grid)) {
-					
-				}
-
-				if (infectR <= infectPercent) {
-					grid[i][j].setSick(true);
-					infectedToday ++;
+				if (hn.check(grid)) {
+					if (infectR <= infectPercent) {
+						grid[i][j].setSick(true);
+						infectedToday ++;
+					}
 				}
 
 				if (grid[i][j].getSick()) {
@@ -60,6 +60,13 @@ public class simulation extends infection {
 			+ totalNumberOfDead  + '\n' + "Today " + peopleRecovered + " people recovered from the infection.");
 
 		day++;
+
+		System.out.println("Hit Enter to continue.");
+		String s = sc.nextLine();
+
+		if (s.equals("")){
+			execute(grid, minSick, maxSick, deathPercent, infectPercent);
+		}
 
  	}
 }
