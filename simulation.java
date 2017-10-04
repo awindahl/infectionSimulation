4d33bd039d072925a5dd11cabd5320bca582bb26
awindahl <awindahl@kth.se>
2017-10-04 17:32:58 +0200

    bug fix, subject cannot get infected and die on same day

--- simulation.java
+++ simulation.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class simulation extends infection {
 
-	int day = 1;
+	int day = 0;
 	int totalNumberOfInfected = 0;
 	int totalNumberOfDead = 0;
 
@@ -26,57 +26,76 @@ public class simulation extends infection {
 
 		Scanner sc = new Scanner(System.in);
 
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid.length; j++) {
+		if (day > 0) {
 
-				double infectR  = 	  rn.nextDouble();
-				double deathR   =	  rn.nextDouble();
-				hasNeighbour hn = new hasNeighbour();
+			for (int i = 0; i < grid.length; i++) {
+				for (int j = 0; j < grid.length; j++) {
 
-				if (infectR <= infectPercent) {
-					if (hn.check(grid, i, j) && !grid[i][j].getSick() && !grid[i][j].getDead()) {
-						grid[i][j].setSick(true);
-						grid[i][j].setDisabled(true);
-						grid[i][j].setTime(randomTime);
-						infectedToday ++;
+					double infectR  = 	  rn.nextDouble();
+					double deathR   =	  rn.nextDouble();
+					hasNeighbour hn = new hasNeighbour();
+
+					if (infectR <= infectPercent) {
+						if (hn.check(grid, i, j) && !grid[i][j].getSick() && !grid[i][j].getDead()) {
+							grid[i][j].setSick(true);
+							grid[i][j].setDisabled(true);
+							grid[i][j].setTime(randomTime);
+							infectedToday ++;
+						}
 					}
-				}
 
-				if (grid[i][j].getSick()) {
+					if (grid[i][j].getSick()) {
 
-					if (deathR <= deathPercent && grid[i][j].isDisabled) {
-						grid[i][j].setDead(true);
-						grid[i][j].setSick(false);
-						System.out.print(" " + d );
-						diedToday++;
+						if (deathR <= deathPercent && !grid[i][j].isDisabled) {
+							grid[i][j].setDead(true);
+							grid[i][j].setSick(false);
+							System.out.print(" " + d );
+							diedToday++;
+						} else {
+
+						if (grid[i][j].getTime() > 0) {
+							grid[i][j].countDown();
+							System.out.print(" " + x);
+						} else {
+
+							grid[i][j].setSick(false);
+							peopleRecovered++;
+							System.out.print(" " + o);
+							}
+						}
 					} else {
-
-					if (grid[i][j].getTime() > 0) {
-						grid[i][j].countDown();
-						System.out.print(" " + x);
-					} else {
-
-						grid[i][j].setSick(false);
-						peopleRecovered++;
+						if (grid[i][j].getDead()) {
+							System.out.print(" " + d);
+						} else {
 						System.out.print(" " + o);
 						}
 					}
-				} else {
-					if (grid[i][j].getDead()) {
-						System.out.print(" " + d);
-					} else {
+				}
+				System.out.println();
+			}
+			for (int i = 0; i < grid.length; i++) {
+				for (int j = 0; j < grid.length; j++) {
+					grid[i][j].setDisabled(false);
+				}
+			}
+		}
+
+	else {
+
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid.length; j++) {
+
+				if (grid[i][j].getSick()) {
+					System.out.print(" " + x);
+					totalNumberOfInfected++;
+				}
+				else {
 					System.out.print(" " + o);
-					}
 				}
 			}
 			System.out.println();
 		}
-
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid.length; j++) {
-				grid[i][j].setDisabled(false);
-			}
-		}
+	}
 
 		totalNumberOfDead += diedToday;
 		totalNumberOfInfected += infectedToday;
